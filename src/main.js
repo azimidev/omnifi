c6ebacef246d0593493a0468224a1d03b196db33
Hassan Azimi <hassanazimi@users.noreply.github.com>
2020-02-16 14:40:42 +0000

    last cleanup

--- src/main.js
+++ src/main.js
@@ -1,22 +1,22 @@
-import Vue from 'vue'
-import * as VueGoogleMaps from 'vue2-google-maps'
-import App from './App.vue'
+import Vue from 'vue';
+import * as VueGoogleMaps from 'vue2-google-maps';
+import App from './App.vue';
 
 Vue.use(VueGoogleMaps, {
-  load: {
-    /*
-     * Usually I put this Google API key in env variables
-     * But this time I didn't because you won't have access to
-     * .env file in version control system like GitHub
-     */
-    key: 'AIzaSyBHTFWKKWvYfxs9rP0fEgLlPo8K2V1MsoM',
-    libraries: 'places',
-  },
-  installComponents: true
-})
+    load: {
+        /*
+         * Usually I put this Google API key in env variables
+         * But this time I didn't because you won't have access to
+         * .env file in version control system like GitHub
+         */
+        key: 'AIzaSyBHTFWKKWvYfxs9rP0fEgLlPo8K2V1MsoM',
+        libraries: 'places',
+    },
+    installComponents: true
+});
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App),
+}).$mount('#app');
