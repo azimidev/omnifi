f3327d1327d78156a415209da4b6aa83c8b4aad0
Hassan Azimi <hassanazimi@users.noreply.github.com>
2020-02-16 02:17:31 +0000

    add a marker component

--- src/components/MapWrapper.vue
+++ src/components/MapWrapper.vue
@@ -7,6 +7,17 @@
             style="width: 100%; height: 500px"
             @click="showInfoWindow = false"
         >
+            <GmapMarker
+                v-for="(location, key) in locations"
+                :key="key"
+                :position="{
+                    lat: parseFloat(location.latitude),
+                    lng: parseFloat(location.longitude)
+                }"
+                :clickable="true"
+                :draggable="true"
+                @click="showDetails(location)"
+            />
         </GmapMap>
     </div>
 </template>
